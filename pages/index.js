0fcefcbd13ddf5f582cd773efa17f6291d08cfc0
JOATLGTM <66840886+JOATLGTM@users.noreply.github.com>
2024-04-05 15:27:15 -0500

    rendered products using Sanity cli

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,35 @@ import React from 'react'
 import { HeroBanner, FooterBanner, Product } from '../components'
 import { client } from '../lib/client';
 
-const Home = () => {
+const Home = ({ productData, bannerData }) => {
+  console.log(productData, bannerData)
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className="products-heading">
         <h2>Best Seling Products</h2>
         <p>Speakers of many variations</p>
       </div>
 
       <div className="products-container">
-        {['Product 1', 'Product'].map(product => product)}
+        {productData?.map(product => product.name)}
       </div>
 
       <FooterBanner />
     </>
   )
+}
+
+// next.js version of useEffect for making async api calls
+export const getServerSideProps = async () => {
+  const productQuery = '*[_type == "product"]'
+  const productData = await client.fetch(productQuery)
+  const bannerQuery = '*[_type == "banner"]'
+  const bannerData = await client.fetch(bannerQuery)
+
+  return {
+    props: { productData, bannerData }
+  }
 }
 
 export default Home
